b86c25c1962d47056230375d98847129ae64c9bd
Anthony Rubbo <anthony.rubbo@gmail.com>
2020-08-29 20:35:01 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,9 +4,9 @@
 def fizzbuzz(num)
   if num / 3
     puts "Fizz"
-  elsif num / 5
+  if num / 5
     puts "Buzz"
-  elsif num / 3 && num / 5
+  if num / 3 && num / 5
     puts "FizzBuzz"
   else 
     puts nil
